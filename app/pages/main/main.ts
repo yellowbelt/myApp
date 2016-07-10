2e7d7c22523a8c9efbab1f16885e6f64600b137b
Olli Muukka <olli.muukka@gmail.com>
2016-07-10 14:52:06 +0300

    UI with tabs

--- app/pages/main/main.ts
+++ app/pages/main/main.ts
@@ -2,9 +2,9 @@ import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 
 @Component({
-  templateUrl: 'build/pages/about/about.html'
+  templateUrl: 'build/pages/main/main.html'
 })
-export class AboutPage {
+export class MainPage {
   constructor(private navController: NavController) {
   }
 }
--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core'
 import {HomePage} from '../home/home';
-import {AboutPage} from '../about/about';
+import {MainPage} from '../main/main';
 import {ContactPage} from '../contact/contact';
 
 @Component({
@@ -16,7 +16,7 @@ export class TabsPage {
     // this tells the tabs component which Pages
     // should be each tab's root Page
     this.tab1Root = HomePage;
-    this.tab2Root = AboutPage;
+    this.tab2Root = MainPage;
     this.tab3Root = ContactPage;
   }
 }
